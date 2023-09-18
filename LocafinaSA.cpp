d4dddd4cf6a88f58312dccaf22e75a3b7e2aeb0f
Ricardo <ricardo.dev1991@gmail.com>
2023-09-18 17:18:10 -0300

    Adicionando feature de incluir um novo cliente

--- LocafinaSA.cpp
+++ LocafinaSA.cpp
@@ -20,20 +20,53 @@ struct Veiculo {
     string loja_retirada;
 };
 
+Cliente incluirCliente(vector<Cliente>& cliente) {
+    Cliente novoCliente;
+    string cpf, nomeCliente, data_nascimento, cnh;
+    char opcao;
+    
+    do {
+    cout << "Digite o seu nome : ";
+    getline(cin, nomeCliente); 
+    novoCliente.nome = nomeCliente;
+
+    cout << "Digite o seu CPF: ";
+    getline(cin, cpf);
+    novoCliente.cpf = cpf;
+
+    cout << "Digite a data de seu nascimento no formato dd/mm/yyyy: ";
+    getline(cin, data_nascimento);
+    novoCliente.DtNascimento = data_nascimento;
+
+    cout << "Digite a sua CNH: ";
+    getline(cin, cnh);
+    novoCliente.cnh = cnh;
+
+    cliente.push_back(novoCliente);
+    cout << "Deseja adicionar um novo cliente? (s/n)";
+    cin >> opcao;
+    cin.ignore();
+
+} while (opcao != 'n');
+
+}
+
 
 int main () {
+    vector<Cliente> cliente;
     int opcao;
     cout << "Escolha uma opcao (de 1 a 5), ou 0 (zero) para sair";
     while(opcao != 0) {
-        cin >> opcao;
+        cin >> opcao;  
+        cin.ignore(); 
         switch (opcao){
         case 1:
             cout << "Incluir um cliente: " << endl;
+            incluirCliente(cliente);
             break;
         case 2:
             cout << "Excluir um cliente: " << endl;
             break;
-
         case 3:
             cout << "Alterar um cliente (CPF): " << endl;
             break;
