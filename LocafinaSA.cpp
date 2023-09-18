7e78d14fc5dccdf3d09e386013c2d2b5331aa017
Tiago Senger <tiagosengerds@gmail.com>
2023-09-18 16:17:16 -0300

    Definição das estruturas de dados e bibliotecas

---
+++ LocafinaSA.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string> 
+struct Cliente{
+    string CPF;
+    string nome;
+    string dtNascimento;
+    string CNH;
+};
+struct Veículo{
+    string Renavan;
+    string placaVeículo;
+    string dtRetirada;
+    string dtEntrega;
+    int hrRetirada;
+    int hrEntrega;
+};
+
+int main(){
+
+    
+
+    return 0;
+}
